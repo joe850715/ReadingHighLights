d8e7b9a97073372a35e90dc8bf5a9688c0cfb4c9
joe850715 <barinhow850715@gmail.com>
2021-12-28 00:11:03 +0800

    update workspace and tomcat 9.0 at localhost-config/server.xml had been changed (because of the change of jre), mention that.

--- jsp_practice/FirstServlet/src/servlet/practice/Hello.java
+++ jsp_practice/FirstServlet/src/servlet/practice/Hello.java
@@ -21,6 +21,10 @@ public class Hello extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		
 		String username = request.getParameter("username");
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String servletPath = request.getServletPath();
+		String pathInfo = request.getPathInfo();
 		
 		PrintWriter out = response.getWriter();
 		out.print("<!DOCTYPE html>");
@@ -30,9 +34,12 @@ public class Hello extends HttpServlet {
 		out.print("</head>");
 		out.print("<body>");
 		out.printf("<h1>Hello!! %s%n</h1>",username);
+		out.printf("<h1>requestURI= %s%n</h1>",requestURI);
+		out.printf("<h1>contextPath= %s%n</h1>",contextPath);
+		out.printf("<h1>servletPath= %s%n</h1>",servletPath);
+		out.printf("<h1>pathInfo= %s%n</h1>",pathInfo);
 		out.print("</body>");
 		out.print("</html>");
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
